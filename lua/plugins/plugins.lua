2e460a42183f13f62ab4b95f0bceb014ce48ce74
Claudio Vasconcelos <ccvasconcelos97@gmail.com>
2024-01-04 11:01:40 +0000

    alpha menu update shortcuts

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -133,7 +133,7 @@ return {
         dashboard.button("1", " " .. " profile-backend", "<cmd> cd ~/Developer/profile-backend <cr><cmd> Neotree <cr>"),
         dashboard.button("2", " " .. " profile-modules", "<cmd> cd ~/Developer/profile-modules <cr><cmd> Neotree <cr>"),
         dashboard.button("3", " " .. " new-work-one",    "<cmd> cd ~/Developer/xing-one <cr><cmd> Neotree <cr>"),
-        dashboard.button("3", " " .. " nvim config",     "<cmd> cd ~/.config/nvim/lua <cr><cmd> Neotree <cr>"),
+        dashboard.button("c", " " .. " nvim config",     "<cmd> cd ~/.config/nvim/lua <cr><cmd> Neotree <cr>"),
         dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
         dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
       }
